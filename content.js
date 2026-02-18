0a12eaa6eae3e40bddcd7f7d63378f1c75762d33
Andy Zhou <zhoua2001@gmail.com>
2026-02-18 17:51:15 -0500

    add changing link in More submenu

--- content.js
+++ content.js
@@ -3,21 +3,33 @@ let tabsContainer = null;
 
 // wait until the tabs are loaded
 const observer = new MutationObserver((mutations) => {
-    let mapsTabExists = false;
-
     for (let mutation of mutations) {
         if (mutation.type === "childList") {
-            for (let node of mutation.addedNodes) {
-                if (node.innerText === "Maps") {
-                    // console.log("Found Maps tab:", node);
-                    mapsTabExists = true;
-                    updateMapsLink(node);
+            for (let outerTab of mutation.addedNodes) {
+                // console.log("Checking added node:", outerTab);
+                
+                // TODO: This runs multiple times when Maps is in the More submenu. Prevent this from running multiple times.
+                if (outerTab.innerText === "Maps") {
+                    // console.log("Found Maps tab:", outerTab);
+                    updateOuterTabMapsLink(outerTab);
+                    break;
+                }
+
+                if (outerTab.innerText.includes("Maps") && outerTab.innerText.includes("More")) {
+                    // console.log("Found Maps tab in More submenu:", outerTab);
+                    let currNode = outerTab;
+                    while (currNode.hasChildNodes() && (currNode.innerText !== "Maps")) {
+                        for (let child of currNode.childNodes) {
+                            if (child.innerText && child.innerText.includes("Maps")) {
+                                currNode = child;
+                                break;
+                            }
+                        }
+                    }
+                    updateMapsLinkInMoreSubmenu(currNode);
                     break;
                 }
             }
-        }
-        if (mapsTabExists) {
-            break;
         }
     }
 });
@@ -25,8 +37,7 @@ const observer = new MutationObserver((mutations) => {
 observer.observe(toolbarContainer, { childList: true, subtree: true });
 
 function updateMapsLink(mapsTabContainer) {
-    let mapsTab = mapsTabContainer.firstChild;
-    let newMapsTab = mapsTab.cloneNode(true);
+    let newMapsTab = mapsTabContainer.cloneNode(true);
 
     const query = new URLSearchParams(window.location.search).get("q") || "";
     let mapsLink = `https://google.com/maps?q=${encodeURIComponent(query)}`;
@@ -35,5 +46,33 @@ function updateMapsLink(mapsTabContainer) {
     newMapsTab.href = mapsLink;
     console.log(newMapsTab);
 
-    mapsTabContainer.replaceChild(newMapsTab, mapsTab);
+    mapsTabContainer.parentNode.replaceChild(newMapsTab, mapsTabContainer);
+}
+
+function updateOuterTabMapsLink(outerTab) {
+    let mapsTab = outerTab.firstChild;
+    let newMapsTab = mapsTab.cloneNode(true);
+
+    const query = new URLSearchParams(window.location.search).get("q") || "";
+    let mapsLink = `https://google.com/maps?q=${encodeURIComponent(query)}`;
+    
+    console.log("Updating Maps tab link to:", mapsLink);
+    newMapsTab.href = mapsLink;
+    console.log(newMapsTab);
+
+    outerTab.replaceChild(newMapsTab, mapsTab);
+}
+
+function updateMapsLinkInMoreSubmenu(mapsTabContainer) {
+    let newMapsTab = mapsTabContainer.cloneNode(true);
+
+    const query = new URLSearchParams(window.location.search).get("q") || "";
+    let mapsLink = `https://google.com/maps?q=${encodeURIComponent(query)}`;
+
+    let mapsLinkContainer = newMapsTab.getElementsByTagName("a")[0];
+    console.log("Updating Maps tab in More submenu link to:", mapsLink);
+    mapsLinkContainer.href = mapsLink;
+    console.log(newMapsTab);
+
+    mapsTabContainer.parentNode.replaceChild(newMapsTab, mapsTabContainer);
 }
